6a88c0976cd53eab9c5b6893203641ed780c86dd
Maksud Nurullaev <maksud.nurullaev@gmail.uz>
2022-05-17 23:31:27 +0500

    Image border coloring

--- src/client/components/ImageThumbnails.vue
+++ src/client/components/ImageThumbnails.vue
@@ -3,7 +3,7 @@
         <p>Нажмите на картинку чтобы увеличить</p>
 
         <a v-for="imageURL in globals.car.images" target="_blank" :href="globals.getWebServiceURL + imageURL">
-            <img :src="globals.getWebServiceURL + imageURL" alt="" style="width:150px">
+            <img :src="globals.getWebServiceURL + imageURL" alt="" style="width:150px" :class="getImageClass(imageURL)">
         </a>
     </div>
     <!-- 
@@ -16,15 +16,35 @@
 import { useGlobalStore } from '../stores/globals';
 const globals = useGlobalStore();
 
+function getImageClass(fileName){
+    if (fileName){
+        var result = fileName.match(/.*-(.*).jpeg/);
+        return (result[1]);    
+    }
+    return 'NaN';
+};
+
+
 </script>
 
 <style scoped>
 img {
-    border: 1px solid #ddd;
+    border: 2px solid #ddd;
     border-radius: 4px;
     padding: 5px;
     width: 150px;
+    margin-right: 3px;
+    margin-top: 3px;
 }
+
+img.In {
+    border: 2px solid green;
+}
+
+img.Out {
+    border: 2px solid blue;
+}
+
 
 img:hover {
     box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
