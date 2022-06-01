82b8defca15dabfa0cf2a054d90a8c5c1379d4c8
Maksud Nurullaev <maksud.nurullaev@gmail.uz>
2022-06-01 05:22:33 +0500

    Add test for user & roles

--- src/server/tests/users/users.test.js
+++ src/server/tests/users/users.test.js
@@ -8,51 +8,56 @@ var tables = [
 ];
 
 function truncate() {
-    // return Promise.all(tables, function (table) {
-    //     return knex.raw('truncate table ' + table + ' cascade');
-    // });
     return knex(User.tableName).truncate();
 };
 
-describe("Test with users:", () => {
-    beforeEach(function () {
+describe("Test:", () => {
+    beforeAll(function () {
         return truncate().then(() => {
-            console.log(' ...trancate all... ');
+            console.log(' ...prepare test - trancate tables... ');
         });
     });
 
-    test(" ... create", () => {
-        // truncate().then(() => {
-            let id1 = 'testuser12@olc.uz';
-            let password1 = 'testuser1@password';
-            expect(id1).toEqual('testuser12@olc.uz');
-            // User.query().delete().then((ttt) => {
-            User.query().insert({
-                id: id1,
-                hashedPassword: myCrypto.hashUserAndPassword(id1, password1)
-            }).then((testUser1) => {
-                // console.log(testUser1);
-                expect(testUser1.id).toEqual(id1);
-                expect(myCrypto.checkUserAndPassword(id1, password1, testUser1.hashedPassword));
-                User.query().delete().where('users.is', testUser1.id).then((user) => {
-                    console.log("Delete test user: " + testUser1.id);
-                });
-            });
-        // });
-        // });
+    let userId1 = 'testuser12@olc.uz',
+        userIdPassword1 = 'testuser1@password',
+        roleId1 = "testRole1",
+        roleId1Desc = "Some description of role1",
+        roleId2 = "testRole2",
+        roleId2Desc = "Some description of role2";
+
+
+    test(" ... user", async () => {
+        expect(userId1).toEqual('testuser12@olc.uz');
+        let user_1 = await User.query().insert({
+            id: userId1,
+            hashedPassword: myCrypto.hashUserAndPassword(userId1, userIdPassword1)
+        });
+        expect(user_1.id).toEqual(userId1);
+        expect(myCrypto.checkUserAndPassword(userId1, userIdPassword1, user_1.hashedPassword));
+
+        let user_2 = await User.query().findById(userId1);
+        expect(user_2).not.toBeNull();
+        expect(myCrypto.checkUserAndPassword(userId1, userIdPassword1, user_2.hashedPassword));
     });
 
-    // beforeEach(() => {
-    //     // User.query().delete();
-    // });
+    test(' ... user & roles ', async () => {
+        let user_1 = await User.query().findById(userId1);
+        expect(user_1).not.toBeNull();
+        let role_1 = await user_1.$relatedQuery('roles').insert({id: roleId1, description: roleId1Desc});
+        expect(role_1).not.toBeNull();
+        expect(role_1.id).toEqual(roleId1);        
+        let role_2 = await user_1.$relatedQuery('roles').insert({id: roleId2, description: roleId2Desc});
+        expect(role_2).not.toBeNull();
+        expect(role_2.id).toEqual(roleId2);        
+
+        let roles = await user_1.$relatedQuery('roles');
+        expect(roles).not.toBeNull();
+        expect(roles.length == 2);
+    });
+
 
     afterAll(() => {
-        console.log("...after all...")
         knex.destroy();
-        // setTimeout(() => {
-            // knex.destroy();
-        // }, 500);
-        // process.exit();
     });
 
 });
