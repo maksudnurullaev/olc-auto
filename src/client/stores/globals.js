b233d9b4b43cfd9d1acf69f5888fa991ab8f9efc
Maksud Nurullaev <maksud.nurullaev@gmail.uz>
2022-06-13 09:00:59 +0500

    Remove debug mode from pinia

--- src/client/stores/globals.js
+++ src/client/stores/globals.js
@@ -10,7 +10,7 @@ const roleRegistered = /^(admin|kpp|1c|registered)/
 export const useGlobalStore = defineStore('globals', {
   state: () => {
     return {
-      debugMode: true,
+      debugMode: false,
       user: {
         id: '',
         role: ''
