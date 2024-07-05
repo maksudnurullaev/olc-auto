95e4291bc8f2c9e69284cab4002e23c261813e00
Maksud Nurullaev <maksud.nurullaev@gmail.com>
2024-07-05 14:23:40 +0500

    Create home page

--- src/client/router/index.js
+++ src/client/router/index.js
@@ -1,12 +1,18 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import Photos from '../views/Photos.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/reception',
     name: 'Photos',
     component: Photos
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
   },
 ]
 
---
+++ src/client/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 style="text-align: center;">Добро пожаловать! ;-)</h1>
+</template>
